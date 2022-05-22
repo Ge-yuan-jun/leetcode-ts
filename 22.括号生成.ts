8d84c57b98286611ee93e778a9dd0e95ac8b45f3
geyuanjun <geyuanjun@bilibili.com>
2022-05-22 13:02:00 +0800

    feat: add generateParenthesis dp solution

--- 22.括号生成.ts
+++ 22.括号生成.ts
@@ -2,10 +2,34 @@
  * @lc app=leetcode.cn id=22 lang=typescript
  *
  * [22] 括号生成
+ * 
+ * https://leetcode.cn/problems/generate-parentheses/solution/by-20182726-ci8b/
  */
 
 // @lc code=start
 function generateParenthesis(n: number): string[] {
+  const dp = [...Array(n+1)].map(() => [])
+
+  dp[0] = ['']
+  dp[1] = ['()']
+
+  for (let i = 2; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      let k = i - 1 - j
+
+      for (let a = 0; a < dp[j].length; a++) {
+        for (let b = 0; b < dp[k].length; b++) {
+          dp[i].push(`(${dp[j][a]})${dp[k][b]}`)
+        }
+      }
+    }
+  }
+
+  return dp[n]
+};
+// @lc code=end
+
+function generateParenthesisDFS(n: number): string[] {
   const ans = []
   const recur = (left, right, n, s) => {
     // terminator
@@ -28,6 +52,4 @@ function generateParenthesis(n: number): string[] {
   }
   recur(0, 0, n, "")
   return ans
-};
-// @lc code=end
-
+}
