7b522df2072b14f502a56f9c863afb9771ed1661
geyuanjun <geyuanjun@bilibili.com>
2022-06-06 22:04:08 +0800

    fix: add 206 comment

--- 206.反转链表.ts
+++ 206.反转链表.ts
@@ -21,10 +21,10 @@ function reverseList(head: ListNode | null): ListNode | null {
   let [cur, prev] = [head, null]
   while (cur) {
     // 步骤分解版
-    // 保留 cur 指针的下一个节点，并改变 cur 的 next 指向
+    // 保留 cur 指针的下一个节点，并改变 cur 的 next 指向，需要一个 next 保存 cur 的下一个元素
     // [next, cur.next] = [cur.next, prev];
     // 两个指针向后挪动一位
-    // [prev, cur] = [cur, cur.next]
+    // [prev, cur] = [cur, next]
     /*************** 最简单版代码 ***************/
     [cur.next, prev, cur] = [prev, cur, cur.next]
   }
