7dd5e1e56501c81e8eb6b06357b4ee5b8350263f
geyuanjun <geyuanjun@bilibili.com>
2022-10-08 14:19:37 +0800

    feat: modify solution

---
+++ 31.下一个排列.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=typescript
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+
+};
+// @lc code=end
+
--- 42.接雨水.ts
+++ 42.接雨水.ts
@@ -6,41 +6,32 @@
 
 // @lc code=start
 /**
- * 单调递减序列
+ * 动态规划
  * @param height 
- * @returns 
  */
 function trap(height: number[]): number {
   const len = height.length
-  let ans = 0
-  if (len <= 2) return 0
-  // 需要计算宽度，所以单调栈里面存储的是 index
-  const stack = []
-  stack.push(0)
-  // 如果是第一个柱子，则不接水
+  const maxLeft = new Array(len).fill(0)
+  const maxRight = new Array(len).fill(0)
+
   for (let i = 1; i < len; i++) {
-    let curHeight = height[i]
-    // 如果比栈顶元素小，则无法形成凹槽
-    if (curHeight < height[stack[stack.length - 1]]) {
-      stack.push(i)
-    } else if (height[stack[stack.length - 1]] === curHeight) {
-      // 如果高度相同，则需要更新右边界
-      stack.pop()
-      stack.push(i)
-    } else {
-      while (stack.length && height[stack[stack.length - 1]] < curHeight) {
-        const left = stack.pop()
-        // 这个地方需要特别处理下，确认数组的长度是否 > 0,否则结果会变成 NaN
-        if (stack.length) {
-          const h = Math.min(height[stack[stack.length - 1]], curHeight) - height[left]
-          const w = i - stack[stack.length - 1] - 1
-          ans += h * w
-        }
-      }
-      stack.push(i)
+    maxLeft[i] = Math.max(height[i-1], maxLeft[i-1])
+  }
+
+  for (let i = len - 2; i >= 0; i--) {
+    maxRight[i] = Math.max(height[i+1], maxRight[i+1])
+  }
+
+  let res = 0
+
+  for (let i = 1; i < len; i++) {
+    const min = Math.min(maxLeft[i], maxRight[i])
+    if (height[i] < min) {
+      res += min - height[i]
     }
   }
-  return ans
+  
+  return res
 }
 // @lc code=end
 /**
@@ -70,6 +61,44 @@ function trap2(height: number[]): number {
     let h = Math.min(leftMax, rightMax) - curHeight
     if (h > 0) {
       ans += h
+    }
+  }
+  return ans
+}
+
+/**
+ * 单调递减序列
+ * @param height 
+ * @returns 
+ */
+function trap3(height: number[]): number{
+  const len = height.length
+  let ans = 0
+  if (len <= 2) return 0
+  // 需要计算宽度，所以单调栈里面存储的是 index
+  const stack = []
+  stack.push(0)
+  // 如果是第一个柱子，则不接水
+  for (let i = 1; i < len; i++) {
+    let curHeight = height[i]
+    // 如果比栈顶元素小，则无法形成凹槽
+    if (curHeight < height[stack[stack.length - 1]]) {
+      stack.push(i)
+    } else if (height[stack[stack.length - 1]] === curHeight) {
+      // 如果高度相同，则需要更新右边界
+      stack.pop()
+      stack.push(i)
+    } else {
+      while (stack.length && height[stack[stack.length - 1]] < curHeight) {
+        const left = stack.pop()
+        // 这个地方需要特别处理下，确认数组的长度是否 > 0,否则结果会变成 NaN
+        if (stack.length) {
+          const h = Math.min(height[stack[stack.length - 1]], curHeight) - height[left]
+          const w = i - stack[stack.length - 1] - 1
+          ans += h * w
+        }
+      }
+      stack.push(i)
     }
   }
   return ans
